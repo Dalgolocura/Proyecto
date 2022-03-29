55f2f5995552ed07ff7bb98766dbf044d8d0fa4e
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-03-29 08:24:51 -0500

    Create ProblemaP1.py

---
+++ ProblemaP1.py
@@ -0,0 +1,12 @@
+"""
+Parte 1 Proyecto DALGO 2022-1
+Autores:
+    Boris Reyes - 202014743
+    Daniel Aguilera - 202010592
+    Juliana Galeano - 202012128
+    William Mendez - 202012662
+"""
+def main():
+    pass
+
+main()
