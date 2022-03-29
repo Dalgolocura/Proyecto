4ce2bac5cb3fa45a16de72c75af26841dbaa94ba
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-03-29 09:47:00 -0500

    Update ProblemaP1.py

--- ProblemaP1.py
+++ ProblemaP1.py
@@ -6,6 +6,26 @@ Autores:
     Juliana Galeano - 202012128
     William Mendez - 202012662
 """
+
+import sys
+
+def lectura():
+    nMatriz = int(sys.stdin.readline())
+
+    while nMatriz != 0:
+        datos = sys.stdin.readline().split(" ")
+        n = int(datos[0])
+        m = int(datos[1])
+        p = int(datos[2])
+        gastoEnergia = sys.stdin.readline().split(" ")
+        portales = []
+
+        for i in range(p):
+            datos = sys.stdin.readline().split(" ")
+            portales.append([(int(datos[0]), int(datos[1])), (int(datos[2]), int(datos[3]))])
+
+        nMatriz -= 1
+
 def main():
     pass
 
